24864cb81852b120dc263c69e4d083ee55339f9e
RendraTriyantoX <43502752+RendraTriyantoX@users.noreply.github.com>
2018-09-23 02:05:56 +0700

    Update Safelink.js

--- Safelink.js
+++ Safelink.js
@@ -18,7 +18,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://webloghintsafelink.blogspot.com/p/generate.htmlurl="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*5)+ 1);
+var xxx=null;
+if(x=="1"){xxx="trik-polosan-telkomsel-gift-paket-2018.html"}
+if(x=="2"){xxx="mod-fornite-android-bypass-device.html"}
+if(x=="3"){xxx="tutorial-cara-unlock-bootloader-xiaomi.html"}
+if(x=="4"){xxx="cara-pasang-twrp-recovery-pada-xiaomi.html"}
+if(x=="5"){xxx="cara-mengatasi-unknown-baseband-dan.html"}a_to_ve[i].href="https://webloghint.blogspot.com/2018/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
